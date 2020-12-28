63aef08a208f5e759366bcf9c1d006ed43122f7f
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2020-12-28 12:39:42 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","lms3");
+$conn = mysqli_connect("localhost","dbuser","dbuserpass","lms3");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
