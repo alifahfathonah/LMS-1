f71ac6969ecaf82f61000e31323d333970f2960e
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2020-12-28 13:46:15 +0500

    Add files via upload

---
+++ layout/footer.php
@@ -0,0 +1,94 @@
+       <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="../logout.php">Logout</a>
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+     <!-- Footer -->
+     
+<footer>
+
+<div class="footer-top py-5">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-bottom-contents-wrap d-flex">
+
+                    <div class="footer-bottom-left d-flex">
+                        <h5 class="text-warning">MY LMS</h5>
+                        <span class="ml-4">Copyright © 2020 LMS. All rights reserved.</span>
+                    </div>
+
+                    <div class="footer-bottom-right flex-grow-1 text-right">
+                        <ul class="footer-bottom-right-links">
+                            <li>
+                                <a href="https://demo.themeqx.com/teachify-lms/public/post/7" class='text-white'>
+                                    Terms of use
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://demo.themeqx.com/teachify-lms/public/post/8" class='text-white'>
+                                    Privacy Policy &amp; Cookie Policy
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+</footer>
+            <!-- End of Footer -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Charts plugins -->
+    <script src="../vendor/chart.js/Chart.min.js"></script>
+    <!-- Charts custom scripts -->
+    <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+
+       <!-- Data Tables plugins -->
+       <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <!-- Data Tables custom scripts -->
+    <script src="../js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
---
+++ layout/sidebar.php
@@ -0,0 +1,123 @@
+   <!-- Sidebar -->
+   <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-chalkboard-teacher"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">LMS</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+    <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Users
+</div>
+
+<!-- Nav Item - Pages Amin Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
+        aria-expanded="true" aria-controls="collapseOne">
+        <i class="fas fa-fw fa-user-tie"></i>
+        <span>Admin</span>
+    </a>
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Admnins:</h6>
+            <a class="collapse-item" href="employees.php"> Add New Admin</a>
+            <a class="collapse-item" href="employee_sale.php">View Admins</a>
+        </div>
+    </div>
+</li>
+<!-- Nav Item - Pages Instructor Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-fw fa-user-tie"></i>
+        <span>Instructors</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Instructors:</h6>
+            <a class="collapse-item" href="employees.php"> Add New Instructor</a>
+            <a class="collapse-item" href="employee_sale.php">View Instructors</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Student Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+        aria-expanded="true" aria-controls="collapseUtilities">
+        <i class="fas fa-fw fa-users"></i>
+        <span>Students</span>
+    </a>
+    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+        data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Students:</h6>
+            <a class="collapse-item" href="customers.php">Add New Student</a>
+            <a class="collapse-item" href="borrow_payment.php">View Students</a>
+        </div>
+    </div>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Inventory
+</div>
+
+<!-- Nav Item - Inventory Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fas fa-fw fa-paste"></i>
+        <span>Workbooks</span>
+    </a>
+    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Workbooks</h6>
+            <a class="collapse-item" href="login.html">Sale</a>
+            <a class="collapse-item" href="register.html">Purchase</a>
+            <a class="collapse-item" href="forgot-password.html">Revenue</a>
+          
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Stocks -->
+<li class="nav-item">
+    <a class="nav-link" style="cursor:pointer" data-toggle="modal" data-target="#logoutModal">
+        <i class="fas fa-fw fa-sign-out-alt"></i>
+        <span>Logout</span></a>
+     
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+
+
+</ul>
+<!-- End of Sidebar -->
