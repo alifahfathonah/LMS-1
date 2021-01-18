e80bb12c24ac137cfb7c38fc841bfafadca445f4
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2021-01-19 00:16:29 +0500

    Update sidebar.php

--- student/layout/sidebar.php
+++ student/layout/sidebar.php
@@ -1,11 +1,11 @@
 
 <!-- Sidebar -->
-   <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+   <ul class="navbar-nav  sidebar sidebar-dark accordion" style="background-color:#252E3A" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-chalkboard-teacher"></i>
+    <div class="sidebar-brand-icon ">
+           <img src="../img/logo.png" style="width:150px" alt="">
     </div>
     <div class="sidebar-brand-text mx-3">LMS</div>
 </a>
